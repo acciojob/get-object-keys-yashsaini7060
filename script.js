678aa77ec7256154be99dfa74bfd635fbba97bc7
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-12-15 22:54:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+// Create an object called student with a property called name
+let student = {
+    name: 'John Doe'
+};
+
+// Add a property to the Object prototype called getKeys
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Now you can call getKeys on any object
+console.log(student.getKeys()); // Outputs: ['name']
